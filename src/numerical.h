fa0d7e4583871425c44d3bca1afa7656f75b8586
Dmitry Lavrukhin <dlavrukhin@yandex.ru>
2022-09-26 01:26:47 +0300

    more understandable code

--- src/numerical.h
+++ src/numerical.h
@@ -3,6 +3,10 @@
 
 
 double minimum_ternary_search(double left, double right, std::function<double(double)> func, double precision = 1e-7) {
+	// finds value in [left, right] which minimizes func
+	// func should have non negative derivative
+	// O(log((right-left)/precision))
+
 	const static double golden_ratio = (std::sqrt(5) + 1) / 2;
 
 	auto is_error_small_enough = [&left, &right, precision]() {
@@ -13,25 +17,25 @@ double minimum_ternary_search(double left, double right, std::function<double(do
 
 	double dist = right - left;
 
-	double lx = right - dist / golden_ratio;
-	double lf = func(lx);
-	double rx = left + dist / golden_ratio;
-	double rf = func(rx);
+	struct MiddlePoint {
+		double x;
+		double y;
+	};
+	auto make_middle_point = [&func](double x) {return MiddlePoint{ x, func(x) }; };
+
+	auto left_mp = make_middle_point(right - dist / golden_ratio);
+	auto right_mp = make_middle_point(left + dist / golden_ratio);
 
 	while (!is_error_small_enough()) {
-		if (lf < rf) {
-			right = rx;
-			rx = lx;
-			rf = lf;
-			lx = right - (right - left) / golden_ratio;
-			lf = func(lx);
+		if (left_mp.y < right_mp.y) {
+			right = right_mp.x;
+			right_mp = left_mp;
+			left_mp = make_middle_point(right - (right - left) / golden_ratio);
 		}
 		else {
-			left = lx;
-			lx = rx;
-			lf = rf;
-			rx = left + (right - left) / golden_ratio;
-			rf = func(rx);
+			left = left_mp.x;
+			left_mp = right_mp;
+			right_mp = make_middle_point(left + (right - left) / golden_ratio);
 		}
 	}
 
