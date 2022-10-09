1e212564eb8613caa97f33f20631aebaf95e5589
Dmitry Lavrukhin <dlavrukhin@yandex.ru>
2022-10-09 18:47:25 +0300

    renamed callback arg

--- src/graphs.h
+++ src/graphs.h
@@ -7,7 +7,7 @@
 void bfs(
 	size_t start_vertex,
 	const std::vector<std::vector<size_t>>& neighbours,
-	std::function<void(size_t)> callback_on_enter,
+	std::function<void(size_t)> callback_on_visit, 
 	std::function<void(std::pair<size_t, size_t>, bool)> callback_on_edge // <from, to>, is_forward_edge
 ) {
 	std::vector<size_t> visited(neighbours.size(), false);
@@ -19,7 +19,7 @@ void bfs(
 		auto vertex = queue.front();
 		queue.pop();
 
-		callback_on_enter(vertex);
+		callback_on_visit(vertex);
 
 		for (const auto child : neighbours[vertex]) {
 			callback_on_edge(std::make_pair(vertex, child), !visited[child]);
